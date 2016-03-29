7e207962c1e476ee887ca85ee7c0f09ed74d35f4
CyborgPotato <TheUnknownWinds@gmail.com>
2016-03-29 19:28:18 -0400

    Setup Changes: Not Properly Updated
    Simple additions of favicon and centre circle

---
+++ main.js
@@ -0,0 +1,9 @@
+﻿function init() {
+    var centreCog = document.getElementById("centre");
+
+    centreCog.style.width = window.innerWidth / 10 + "px";
+    centreCog.style.height = window.innerWidth / 10 + "px";
+
+    centreCog.style.left = window.innerWidth / 2 + "px";
+    centreCog.style.top = window.innerHeight / 2 + "px";
+}
